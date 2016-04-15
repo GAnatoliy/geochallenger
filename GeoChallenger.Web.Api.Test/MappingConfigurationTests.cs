f02103bcd45457d3f74c3b7dbb0cb5fc5c4b3b89
Anatoliy Guts <Anatoliy@mifprojects.com>
2016-04-15 22:55:27 +0300

    Add unittests for mappings

---
+++ GeoChallenger.Web.Api.Test/MappingConfigurationTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+
+namespace GeoChallenger.Web.Api.Test
+{
+    /// <summary>
+    /// Tests mapping configuration.
+    /// </summary>
+    [TestFixture]
+    public class MappingConfigurationTests
+    {
+        [Test]
+        public void TestMappings()
+        {
+            var configuration = MapperConfig.CreateMapperConfiguration();
+            configuration.AssertConfigurationIsValid();
+        }
+    }
+}
