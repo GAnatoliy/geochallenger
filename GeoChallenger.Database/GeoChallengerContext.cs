e56b831c5aa27cb1507f3f4ca55b8db6f5dca33a
Anatoliy Guts <Anatoliy@mifprojects.com>
2016-04-16 12:17:45 +0300

    Add database project

---
+++ GeoChallenger.Database/GeoChallengerContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using GeoChallenger.Domains;
+
+
+namespace GeoChallenger.Database
+{
+    public class GeoChallengerContext: DbContext
+    {
+        public DbSet<Poi> Pois { get; set; }
+    }
+}
